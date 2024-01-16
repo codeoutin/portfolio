6fde22c33aeebe2b001d1bfba7d6176a2c341fcf
Patrick Steger <ps@psteger.com>
2024-01-16 15:39:04 +0700

    Fix 2024

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -155,8 +155,8 @@ function Skills() {
               >
                 My Tech Stack
               </Typography>
-              <Typography itemProp="knowsAbout" className={classes.paragraph}>
-              Full-Stack Development Expertise, Effective Communicator, Project Management and leading roles & Mentoring
+              <Typography itemProp="knowsAbout">
+                Full-Stack Development Expertise, Effective Communicator, Project Management and leading roles & Mentoring
               </Typography>
               <div
               // ref={intersectionExitRef}
